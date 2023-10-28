63fcb068494bdc950c298dcb3b4837f0651f67d4
JoseRegueiro17 <132882721+JoseRegueiro17@users.noreply.github.com>
2023-10-28 15:32:25 +0000

    ejercicio completo

---
+++ src/js/component/newnavbar.jsx
@@ -0,0 +1,30 @@
+
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const Newnavbar = () => {
+    return (
+      <>
+    <Navbar expand="lg" className="navbar navbar-dark bg-dark">
+      <Container fluid>
+        <Navbar.Brand href="#">Start Bootstrap</Navbar.Brand>
+          <Nav
+            className="col me-auto mt-3 my-2 my-lg-0 justify-content-end"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#" className="nav-link active">Home</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Services</Nav.Link>
+            <Nav.Link href="#">Contact</Nav.Link>
+          </Nav>
+      </Container>
+    </Navbar>
+    </>
+  );
+};
+
+    export default Newnavbar;
